858087da3f07d0f38aab1ef3b88965981eb6bbe3
ziadseleem <ziad32seleem@gmail.com>
2026-06-22 13:01:39 +0300

    feat: add scroll-to-top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Layout from './components/layout/Layout';
 import HomePage from './pages/HomePage';
 import CollectionsPage from './pages/CollectionsPage';
@@ -6,10 +7,21 @@ import DeveloperPage from './pages/DeveloperPage';
 import PublisherPage from './pages/PublisherPage';
 import SmoothScrollbar from './components/shared/SmoothScrollbar';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.dispatchEvent(new CustomEvent('nexusScrollToTop'));
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <div className="h-screen w-screen overflow-hidden bg-background">
       <SmoothScrollbar className="h-full w-full main-scroll-container">
+        <ScrollToTop />
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<HomePage />} />
